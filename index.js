1223621087fffa29957f0572efed776a25291019
Sherry <sherryptk@gmail.com>
2017-09-21 13:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ function takeANumber(katzDeliLine, name){
 
 }
 
+function nowServing(katzDeliLine){
+
+  var name = katzDeliLine[0]
+
+  katzDeliLine.unshift()
+
+  return name
+  
+}
+
+// Build a function nowServing. This function should return the first person in line and then remove that 
+// individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
+
+
 // for (var i = 0; i < name.length; i++) {
 //   array.push('Welcome, ' name[i] + '. You are number ' + katzDeliLine[i]+1 + ' in line.')
 // }
 //return array
 
-// The function should return their position in line. And don't go being too programmer-y and give them their index.
-// If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 
-//Welcome, Ada. You are number 1 in line.
