860e0381572bb27172fb95d7bc3e1f99cf46fbbe
Sherry <sherryptk@gmail.com>
2017-09-21 13:22:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function takeANumber(katzDeliLine, name){
 
-  var line = ('Welcome, ' name + '. You are number ' + (katzDeliLine.length+1) + ' in line.')
+  var line = 'Welcome, ' name + '. You are number ' + (katzDeliLine.length+1) + ' in line.'
 
   katzDeliLine.push(name)
 
   return line
 
-  // for (var i = 0; i < name.length; i++) {
-  //   array.push('Welcome, ' name[i] + '. You are number ' + katzDeliLine[i]+1 + ' in line.')
-  // }
-  //return array
 }
+
+// for (var i = 0; i < name.length; i++) {
+//   array.push('Welcome, ' name[i] + '. You are number ' + katzDeliLine[i]+1 + ' in line.')
+// }
+//return array
 
 // The function should return their position in line. And don't go being too programmer-y and give them their index.
 // If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
