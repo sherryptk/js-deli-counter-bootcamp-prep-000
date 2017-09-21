e71f00cb9eb32b7f1eb4837841dee137e01a756a
Sherry <sherryptk@gmail.com>
2017-09-21 13:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,26 @@ function nowServing(katzDeliLine){
 
 }
 
-// Build a function nowServing. This function should return the first person in line and then remove that
-// individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
+// Build a function currentLine that returns the current line. For example, if katzDeliLine is currently ["Ada", "Grace"], 
+// currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". If there is nobody in line, 
+// it should return "The line is currently empty."
+
+function currentLine(katzDeliLine){
+
+var array = []
+  if (katzDeliLine.length>0) {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      count = i+1
+      array.push(count + '. ' + katzDeliLine[i])
+    }
+      return "The line is currently: " + array
+
+    }
+    else {
+      return 'The line is currently empty.'
+    }
+    
+  }
 
 
 // for (var i = 0; i < name.length; i++) {
